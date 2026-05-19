81e8f637f8f80d6cbf8c3dd7c6fcd2639da8a4c5
Quentin McGuigan <qmcguigan959@gmail.com>
2026-05-19 14:51:56 +0100

    Update secretKey to use a placeholder
    Replaced secret key with a placeholder for security.

--- urlSafetyBE/globals.py
+++ urlSafetyBE/globals.py
@@ -3,7 +3,7 @@ from flask_mail import Mail
 import os
 #imports
 
-secretKey = 'Cl1ffjumper?'
+secretKey = '###########'
 
 #client = MongoClient('mongodb://127.0.0.1:27017')
 cloud = os.environ.get('cloudDBKey')
